2900f886292d5fe863e1a2463d1f55ede96d2bf4
Bolaji Hammed <hammedbolajihammed@gmail.com>
2023-07-04 19:09:46 +0100

    file modified for build

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import './style.css'
+import './css/bootstrap.min.css'
+import './css/font-awesome.min.css'
+import './css/templatemo_misc.css'
+import './css/templatemo_style.css'
+import App from './App.vue'
+
+createApp(App).mount('#app')
